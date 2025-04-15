c57c3f4faec53be067b364d0ebc3795f0a1154fa
Sam Sh <saby24@student.bth.se>
2025-04-15 22:10:05 +0200

    Starting kmom02, creating files, testing sessions and flash messages.

---
+++ src/Card/CardGraphic.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Card;
+
+class CardGraphic
+{
+    // private $suit;
+    // private $rank;
+
+    // public function __construct() {
+    //     $this->$suit = null;
+    //     $this->$rank = null;
+    // }
+}
---
+++ src/Controller/CardGameController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Card\Card;
+use App\Card\CardGraphic;
+use App\Card\CardHand;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardGameController extends AbstractController
+{
+    #[Route("/test", name: "test_session")]
+    public function sessionTest(
+        SessionInterface $session
+    ): Response {
+        $session->set("test", "Hej jag är en session");
+        $session->set("test2", "Hello, här är det ett annat test");
+
+        return $this->redirectToRoute("show_session");
+    }
+
+    #[Route("/session", name: "show_session")]
+    public function session(
+        SessionInterface $session
+    ): Response {
+
+        $data = [
+            "test" => $session->get("test"),
+            "test2" => $session->get("test2")
+        ];
+
+        return $this->render('Cards/session.html.twig', $data);
+    }
+
+    #[Route("/session/delete", name: "delete_session")]
+    public function deleteSession(
+        SessionInterface $session
+    ): Response {
+
+        $session->clear();
+
+        $this->addFlash(
+            'notice',
+            'The session is deleted!'
+        );
+
+        return $this->redirectToRoute('show_session');
+    }
+}
