3bfa591d9842a6d4538ba83c50c7598421fa8f10
Sam Sh <saby24@student.bth.se>
2025-04-19 00:39:27 +0200

    Implement POST route to draw specified number of cards and return remaining deck in JSON

--- src/Controller/ControllerJsonCardGame.php
+++ src/Controller/ControllerJsonCardGame.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Card\DeckOfCards;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,20 +28,11 @@ class ControllerJsonCardGame extends AbstractController
         return $response;
     }
 
-
     #[Route("api/deck/shuffle", name: "api-deck-shuffle-get", methods: ["GET"])]
     public function viewShuffleForm(): Response
     {
         return $this->render('Cards/json_form.html.twig');
     }
-
-
-    #[Route("api/deck/draw", name: "api-deck-draw-get", methods: ["GET"])]
-    public function apiDeckDrawForm(): Response
-    {
-        return $this->render('Cards/json_form.html.twig');
-    }
-
 
     #[Route("api/deck/shuffle", name: "api-deck-shuffle", methods: ["POST"])]
     public function apiDeckShuffle(SessionInterface $session): Response
@@ -56,6 +48,12 @@ class ControllerJsonCardGame extends AbstractController
 
     return $response;
 }
+
+    #[Route("api/deck/draw", name: "api-deck-draw-get", methods: ["GET"])]
+        public function apiDeckDrawForm(): Response
+        {
+            return $this->render('Cards/json_form.html.twig');
+        }
 
     #[Route("api/deck/draw", name: "api-deck-draw", methods: ["POST"])]
     public function apiDeckDraw(SessionInterface $session): Response
@@ -75,6 +73,39 @@ class ControllerJsonCardGame extends AbstractController
         return $response;
     }
 
+
+    #[Route("api/deck/draw/form", name: "api-deck-draw-cards-form", methods: ["POST"])]
+    public function drawCardsForm(): Response {
+        return $this->render('Cards/api-draw-cards-form.html.twig');
+    }
+
+    #[Route("api/deck/draw/cards", name: "api-deck-draw-cards", methods: ["POST"])]
+    public function drawCardsNumber(
+        Request $request,
+        SessionInterface $session
+    ): JsonResponse {
+        $number = $request->request->get('num_cards');
+        $cards = $session->get("shuffled_cards_json", []);
+
+        $drawn = [];
+        $count = 0;
+
+    while ($count < $number && !empty($cards)) {
+        $drawn[] = array_pop($cards);
+        $count++;
+    }
+
+        $session->set("shuffled_cards_json", $cards);
+
+        $data = [
+            'drawn_cards' => $drawn,
+            'remaining_cards' => count($cards)
+        ];
+
+        $response = $this->responseHelper($data);
+        return $response;
+
+    }
 
     private function responseHelper(array $data): JsonResponse
     {
