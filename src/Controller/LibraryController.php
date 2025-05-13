d106e77dd10e7cbdfb97cb4d14357673f8d3de51
Sam Sh <saby24@student.bth.se>
2025-05-13 22:46:56 +0200

    Add routes and template to create books

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Book;
+// use Doctrine\ORM\Mapping\PostPersist;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +17,32 @@ final class LibraryController extends AbstractController
         return $this->render('library/index.html.twig', [
             'controller_name' => 'LibraryController',
         ]);
+    }
+
+    #[Route('/library/create', name: 'show-create-form', methods: ['GET'])]
+    public function showCreateForm()
+    {
+        return $this->render('library/create.html.twig');
+    }
+
+    #[Route('/library/create', name: 'library_create', methods: ['POST'])]
+    public function createBook(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $book = new Book();
+
+        $book->setTitle($request->request->get('title'));
+        $book->setIsbn($request->request->get('isbn'));
+        $book->setAuthor($request->request->get('author'));
+        $book->setImage($request->request->get('image'));
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        dump($book);
+
+
+        return new Response('Saved new book with id ' . $book->getId());
     }
 }
